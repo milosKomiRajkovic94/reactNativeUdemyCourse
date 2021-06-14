e77610e0ee4e4aadab97811a44f4c83a618ad78b
Miloš Rajković <rajkovicm94@gmail.com>
2021-06-14 11:02:51 +0200

    Eleventh chapter implementation.

--- food/src/components/ResultsList.js
+++ food/src/components/ResultsList.js
@@ -1,18 +1,31 @@
 import React from 'react';
 
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
 import { FlatList } from 'react-native-gesture-handler';
+import ResultsDetail from './ResultsDetail';
 
-const ResultsList = ({title, results}) => {
+import {withNavigation} from 'react-navigation';
+
+const ResultsList = ({title, results, navigation}) => {
+
+    if(!results.length){
+        return null;
+    }
+
     return(
-        <View>
+        <View style={styles.container}>
             <Text style={styles.title}>{title}</Text>
             <FlatList
                 horizontal={true}
                 data={results}
+                showsHorizontalScrollIndicator={false}
                 keyExtractor={(results) => results.id}
                 renderItem={({ item }) => {
-                return <Text>{item.name}</Text>
+                return(
+                    <TouchableOpacity onPress={() => navigation.navigate("ResultsShow", { id: item.id})}> 
+                        <ResultsDetail result={item} />
+                    </TouchableOpacity>
+                )
                 }}
             />
         </View>
@@ -22,8 +35,12 @@ const ResultsList = ({title, results}) => {
 const styles = StyleSheet.create({
     title: {
         fontSize: 18,
-        fontWeight: 'bold'
+        fontWeight: 'bold',
+        marginLeft: 15,
+    },
+    container: {
+        marginBottom: 10,
     }
 });
 
-export default ResultsList;
+export default withNavigation(ResultsList);
--- food/src/screens/SearchScreen.js
+++ food/src/screens/SearchScreen.js
@@ -1,13 +1,13 @@
 import React, {useState, useEffect} from 'react';
 
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, ScrollView } from 'react-native';
 import SearchBar from '../components/SearchBar';
 
 import useResults from '../hooks/useResults';
 
 import ResultsList from "../components/ResultsList";
 
-const SearchScreen = () => {
+const SearchScreen = ({ navigation }) => {
     const [term, setTerm] = useState('');
     const [searchApi, results, errorMessage] = useResults();
 
@@ -19,21 +19,27 @@ const SearchScreen = () => {
     }
 
     return(
-        <View style={styles.backgroundStyle}>
+        <View style={{flex: 1, backgroundColor: '#ffffff', height: '100%'}}>
             <SearchBar term={term} onTermChange={setTerm} onTermSubmit={searchApi}/>
               {errorMessage ? <Text> {errorMessage} </Text> : null} 
             <Text>We have found {results.length} results</Text>
-            <ResultsList results={filterResultsByPrice('$')} title="Cost effective" />
-            <ResultsList results={filterResultsByPrice('$$')} title="Bit Pricier" />
-            <ResultsList results={filterResultsByPrice('$$$')} title="Big Spender" />
+            <ScrollView>
+                <ResultsList 
+                    results={filterResultsByPrice('$')} 
+                    title="Cost effective" 
+                />
+                <ResultsList 
+                    results={filterResultsByPrice('$$')} 
+                    title="Bit Pricier" 
+                />
+                <ResultsList 
+                    results={filterResultsByPrice('$$$')} 
+                    title="Big Spender" 
+                />
+            </ScrollView>
         </View>
     )
 };
 
-const styles = StyleSheet.create({
-    backgroundStyle: {
-        backgroundColor: '#fff',
-        height: '100%'
-    }
-});
+const styles = StyleSheet.create({});
 export default SearchScreen;
