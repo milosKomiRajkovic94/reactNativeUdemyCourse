51042211227d8cd17a789e43f386eae8e565923f
Milos Rajkovic <mrajkovic@jiway.rs>
2021-10-08 18:44:24 +0200

    New changes

--- track-server/src/routes/authRoutes.js
+++ track-server/src/routes/authRoutes.js
@@ -5,43 +5,39 @@ const User = mongoose.model('User');
 
 const router = express.Router();
 
-router.post('/signup', async(req, res) => {
-    console.log(req.body);
-    const {email, password} = req.body;
+router.post('/signup', async (req, res) => {
+  const { email, password } = req.body;
 
-    try{
-        const user = new User({email, password});
-        await user.save();
+  try {
+    const user = new User({ email, password });
+    await user.save();
 
-        const token = jwt.sign({ userId: user._id }, 'MY_SECRET_KEY');
-        res.send({ token });
-    }catch(err){
-        res.status(422).send(err.message);
-    }
+    const token = jwt.sign({ userId: user._id }, 'MY_SECRET_KEY');
+    res.send({ token });
+  } catch (err) {
+    return res.status(422).send(err.message);
+  }
 });
 
-
 router.post('/signin', async (req, res) => {
-    const {email, password} = req.body; 
+  const { email, password } = req.body;
 
-    if(!email || !password){
-        return res.status(422).send({ error: 'Must provide email and password'});
-    }
+  if (!email || !password) {
+    return res.status(422).send({ error: 'Must provide email and password' });
+  }
 
-    const user = await User.findOne({ email });
+  const user = await User.findOne({ email });
+  if (!user) {
+    return res.status(422).send({ error: 'Invalid password or email' });
+  }
 
-    if(!user){
-        return res.status(422).send({error: 'Email not found'});
-    }
-
-    try{
-        await user.comparePassword(password);
-
-        const token = jwt.sign({ userId: user._id }, 'MY_SECRET_KEY');
-        res.send({ token });
-    }catch(err){
-        return res.status(422).send({ error: 'Invalid password or email'});
-    }
+  try {
+    await user.comparePassword(password);
+    const token = jwt.sign({ userId: user._id }, 'MY_SECRET_KEY');
+    res.send({ token });
+  } catch (err) {
+    return res.status(422).send({ error: 'Invalid password or email' });
+  }
 });
 
 module.exports = router;
--- track-server/src/routes/trackRoutes.js
+++ track-server/src/routes/trackRoutes.js
@@ -1,6 +1,6 @@
-const express = require('express'); 
+const express = require('express');
 const mongoose = require('mongoose');
-const requireAuth = require('../middlewarres/requireAuth');
+const requireAuth = require('../middlewares/requireAuth');
 
 const Track = mongoose.model('Track');
 
@@ -9,27 +9,27 @@ const router = express.Router();
 router.use(requireAuth);
 
 router.get('/tracks', async (req, res) => {
-    const tracks = await Track.find({ userId: req.user._id});
+  const tracks = await Track.find({ userId: req.user._id });
 
-    res.send(tracks);
+  res.send(tracks);
 });
 
 router.post('/tracks', async (req, res) => {
-    const { name, locations } = req.body;
+  const { name, locations } = req.body;
 
-    if(!name || !locations) {
-        return res.status(422).send({ error: 'You must provide a name and locations' });
-    }
+  if (!name || !locations) {
+    return res
+      .status(422)
+      .send({ error: 'You must provide a name and locations' });
+  }
 
-
-
-    try{
-        const track = new Track({ name, locations, userId: req.user._id });
-        await track.save();
-        res.send(track);    
-    }catch(err) {
-        res.status(422).send({error: err.message})
-    }
+  try {
+    const track = new Track({ name, locations, userId: req.user._id });
+    await track.save();
+    res.send(track);
+  } catch (err) {
+    res.status(422).send({ error: err.message });
+  }
 });
 
 module.exports = router;
